340733929db8d4413f95fa722203415edfb621e0
Shankar Dutta <shankardutta1719@gmail.com>
2025-08-06 10:29:26 +0530

    project setup done !!

---
+++ src/components/customui/ThemeToggleButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { MoonStar, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "../shadcnui/button";
+
+const ThemeToggleButton = () => {
+	const { theme, setTheme } = useTheme();
+
+	return (
+		<Button
+			onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+			className="flex cursor-pointer items-center">
+			<Sun
+				size={28}
+				className="-rotate-90 opacity-100 transition-all duration-300 dark:rotate-0 dark:opacity-0"
+			/>
+
+			<MoonStar
+				size={28}
+				className="absolute -rotate-90 opacity-0 transition-all duration-300 dark:rotate-0 dark:opacity-100"
+			/>
+		</Button>
+	);
+};
+
+export default ThemeToggleButton;
