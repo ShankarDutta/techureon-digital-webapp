79f705115ea6b56c2d910f1f514080481fbc35bb
Shankar Dutta <shankardutta1719@gmail.com>
2025-08-11 08:55:22 +0530

    Nav Bar done !!

--- src/components/Header/NavBar.tsx
+++ src/components/Header/NavBar.tsx
@@ -7,8 +7,8 @@ import MobileNav from "./MobileNav";
 
 const NavBar = () => {
 	return (
-		<section className="fixed right-0 left-0 mx-auto w-full max-w-7xl px-6">
-			<div className="mt-6 flex items-center justify-between rounded-md bg-black/6 px-6 pb-2 backdrop-blur-lg md:px-8 lg:rounded-4xl dark:bg-white/8">
+		<header className="fixed right-0 left-0 mx-auto w-full max-w-7xl px-6">
+			<nav className="mt-6 flex items-center justify-between rounded-md bg-black/6 px-6 pb-2 backdrop-blur-lg md:px-8 lg:rounded-4xl dark:bg-white/8">
 				<div className="mt-1 flex items-center">
 					{/* Logo  */}
 					<Link href={"/"}>
@@ -37,8 +37,8 @@ const NavBar = () => {
 				<Button className="hover: mt-2 hidden bg-blue-500 text-white hover:cursor-pointer hover:bg-blue-600 lg:block">
 					Schedule a Call
 				</Button>
-			</div>
-		</section>
+			</nav>
+		</header>
 	);
 };
 
