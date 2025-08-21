48a35129d72d64ede2f6be917c69236c6508cab6
Shankar Dutta <shankardutta1719@gmail.com>
2025-08-21 20:57:02 +0530

    Blog Page Hero Add !!

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,11 @@
+import BlogHero from "@/components/Hero/BlogHero";
+
 const page = () => {
-	return <>Enter</>;
+	return (
+		<section className="mx-auto max-w-7xl px-6">
+			<BlogHero />
+		</section>
+	);
 };
 
 export default page;
