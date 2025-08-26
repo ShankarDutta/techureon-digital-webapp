228ee6d981ae78d53a540b9bc12294f66d0ad99c
Shankar Dutta <shankardutta1719@gmail.com>
2025-08-26 10:45:35 +0530

    blog page completed !!

--- src/app/blog/[slug]/layout.tsx
+++ src/app/blog/[slug]/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function BlogSlugLayout({ children }: { children: ReactNode }) {
 	return (
-		<section className="mx-auto max-w-4xl px-4 pt-36 pb-14">
+		<section className="mx-auto max-w-7xl px-4 pt-36 pb-14">
 			{children}
 		</section>
 	);
